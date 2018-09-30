7b4ee6f6a3c2ba1c051042be2b1096434b468b97
Ray Ventura <circfox@yahoo.com>
2018-09-29 19:09:44 -0700

    added new file, modified files

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers(
-  id INT(10) NOT NULL AUTO INCREMENT,
-  burger_name STRING(25) NOT NULL,
+CREATE TABLE burgers
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
--- server.js
+++ server.js
@@ -6,9 +6,14 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
+// Sets up the Express App
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
 app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({
@@ -22,4 +27,13 @@ app.engine("handlebars", exphbs({
 }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/")
+// Routes
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+var routes = require("./controllers/burgers_controller.js");
+app.use("/", routes);
+
+// Starts the server to begin listening
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+app.listen(PORT, function(){
+  console.log("App listening on PORT " + PORT);
+});
