a00276902677bd0cc03eff5111e2ad896c0f9c7d
Ray Ventura <circfox@yahoo.com>
2018-10-08 18:30:01 -0700

    modified files

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db" 
-});
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
+
 
 // Make connection.
-connection.connect(function(err) {
-  if(err) {
+connection.connect(function (err) {
+  if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
